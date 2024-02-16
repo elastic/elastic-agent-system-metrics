f074726a310920637f4a191f37a6640a91c49753
fearful-symmetry <alexpkristiansen@icloud.com>
2024-02-15 16:13:23 -0800

    still tinkering

--- metric/system/process/process_linux_test.go
+++ metric/system/process/process_linux_test.go
@@ -93,6 +93,11 @@ func TestFetchProcessFromOtherUser(t *testing.T) {
 		require.NoError(t, err)
 		t.Logf("Got users: %s", string(out))
 
+		cmd = exec.Command("groups")
+		out, err = cmd.CombinedOutput()
+		require.NoError(t, err)
+		t.Logf("Got groups: %s", string(out))
+
 		uid, err := CreateUser("test", 1000)
 		require.NoError(t, err)
 		t.Logf("uid: %v", uid)
