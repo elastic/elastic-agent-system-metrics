18f85290253580679f3745dae7885169f3b64842
Vihas Makwana <vihas.makwana@elastic.co>
2025-03-19 20:41:22 +0530

    use GetActiveProcessorCount

--- metric/system/numcpu/cpu_windows.go
+++ metric/system/numcpu/cpu_windows.go
@@ -18,92 +18,17 @@
 package numcpu
 
 import (
-	"encoding/binary"
-	"errors"
-	"unsafe"
+	"fmt"
 
 	"golang.org/x/sys/windows"
 )
 
-var (
-	modkernel32 = windows.NewLazySystemDLL("kernel32.dll")
-
-	getLogicalProcessorInformationEx = modkernel32.NewProc("GetLogicalProcessorInformationEx")
-)
-
-type systemLogicalProcessorInformationEx struct {
-	Relationship uint32
-	Size         uint32
-}
-
-type groupRelationship struct {
-	MaximumGroupCount uint16
-	ActiveGroupCount  uint16
-	Reserved          [20]uint8
-	// variable size array of processorGroupInfo
-	ProcesorGrupInfo []processorGroupInfo
-}
-
-type processorGroupInfo struct {
-	MaximumProcessorCount byte
-	ActiveProcessorCount  byte
-	Reserved              [38]byte
-	ActiveProcessorMask   uint64
-}
-
-func convertByteTpProcessorInformationStruct(data []byte) systemLogicalProcessorInformationEx {
-	return systemLogicalProcessorInformationEx{
-		Relationship: binary.LittleEndian.Uint32(data),
-		Size:         binary.LittleEndian.Uint32(data[4:]),
-	}
-}
-
-func convertByteToGroupStruct(data []byte) groupRelationship {
-	group := groupRelationship{}
-	group.MaximumGroupCount = binary.LittleEndian.Uint16(data)
-	group.ActiveGroupCount = binary.LittleEndian.Uint16(data[2:4])
-	if group.ActiveGroupCount > 0 {
-		groups := make([]processorGroupInfo, group.ActiveGroupCount)
-		index := 24 // need to account for 20 reserved bytes
-		for i := uint16(0); i < group.ActiveGroupCount; i++ {
-			groups[i].MaximumProcessorCount = data[index]
-			index += 1
-			groups[i].ActiveProcessorCount = data[index]
-			index += 1 + 38 // need to account 38 reserved bytes
-			groups[i].ActiveProcessorMask = binary.LittleEndian.Uint64(data[index:])
-			index += 8
-		}
-		group.ProcesorGrupInfo = groups
-	}
-	return group
-}
-
 // getCPU implements NumCPU on windows
 // For now, this is a bit of a hack that just asks for per-CPU performance data, and reports the CPU count
 func getCPU() (int, bool, error) {
-	var bufLen uint32 = 0
-	_, _, err := getLogicalProcessorInformationEx.Call(uintptr(4), uintptr(0), uintptr(unsafe.Pointer(&bufLen)))
-	if err != nil && !errors.Is(err, windows.ERROR_INSUFFICIENT_BUFFER) {
-		return -1, false, err
+	numCpu := windows.GetActiveProcessorCount(0xffff)
+	if numCpu == 0 {
+		return -1, false, fmt.Errorf("received an error while fetching cpu count: %w", windows.GetLastError())
 	}
-	if bufLen == 0 {
-		return -1, false, windows.ERROR_INVALID_FUNCTION
-	}
-	buf := make([]byte, bufLen)
-	_, _, err = getLogicalProcessorInformationEx.Call(uintptr(4), uintptr(unsafe.Pointer(&buf[0])), uintptr(unsafe.Pointer(&bufLen)))
-	if err != nil && !errors.Is(err, windows.ERROR_SUCCESS) {
-		return -1, false, err
-	}
-	index := 0
-	numCpus := 0
-	for index < int(bufLen) {
-		processorInformation := convertByteTpProcessorInformationStruct(buf[index:])
-		group := convertByteToGroupStruct(buf[index+8:]) // the parent struct is of 8 bytes
-		index += int(processorInformation.Size)
-		for _, groupInfo := range group.ProcesorGrupInfo {
-			// count cores per processor group
-			numCpus += int(groupInfo.ActiveProcessorCount)
-		}
-	}
-	return numCpus, true, nil
+	return int(numCpu), true, nil
 }
