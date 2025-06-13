7d9beccd02c7c4a955f5421a565074cfdc9a9748
kruskall <99559985+kruskall@users.noreply.github.com>
2025-06-13 14:46:21 +0200

    feat: replace global logger where possible
    replace global logger with local logger

--- report/metrics_report_test.go
+++ report/metrics_report_test.go
@@ -24,14 +24,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/elastic-agent-libs/logp/logptest"
 	"github.com/elastic/elastic-agent-libs/monitoring"
 )
 
 func TestSystemMetricsReport(t *testing.T) {
-	_ = logp.DevelopmentSetup()
-	logger := logp.L()
-	err := SetupMetrics(logger, "TestSys", "test")
+	err := SetupMetrics(logptest.NewTestingLogger(t, ""), "TestSys", "test")
 	require.NoError(t, err)
 
 	var gotCPU, gotMem, gotInfo bool
