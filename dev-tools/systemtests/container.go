8f57a3cf531dc4990c9a5c330e8fc1cbb02c2229
kruskall <99559985+kruskall@users.noreply.github.com>
2025-06-13 20:39:09 +0200

    Update container.go

--- dev-tools/systemtests/container.go
+++ dev-tools/systemtests/container.go
@@ -247,9 +247,6 @@ func (tr *DockerTestRunner) createTestContainer(ctx context.Context, apiClient *
 	gomodcacheValue = bytes.TrimSuffix(gomodcacheValue, []byte("\n"))
 	require.NotEmpty(tr.Runner, gomodcacheValue)
 
-	_, err = os.Stat(string(gomodcacheValue))
-	require.NoError(tr.Runner, err, fmt.Sprintf("'%s'", gomodcacheValue))
-
 	resp, err := apiClient.ContainerCreate(ctx, &container.Config{
 		Image:      tr.Container,
 		Cmd:        testRunCmd,
