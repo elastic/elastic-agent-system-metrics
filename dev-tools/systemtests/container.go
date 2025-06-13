7a2add04ab7f1a8cda049a4e318d1385fc14c08f
kruskall <99559985+kruskall@users.noreply.github.com>
2025-06-13 16:29:49 +0200

    fix: do not quote wdPath in single quote

--- dev-tools/systemtests/container.go
+++ dev-tools/systemtests/container.go
@@ -215,7 +215,7 @@ func (tr *DockerTestRunner) createTestContainer(ctx context.Context, apiClient *
 	_, err = io.Copy(os.Stdout, reader)
 	require.NoError(tr.Runner, err, "error copying image")
 
-	wdCmd := exec.Command("go", "list", "-m", "-f", "'{{.Dir}}'")
+	wdCmd := exec.Command("go", "list", "-m", "-f", "{{.Dir}}")
 	wdPath, err := wdCmd.CombinedOutput()
 	require.NoError(tr.Runner, err, "error finding root path")
 
