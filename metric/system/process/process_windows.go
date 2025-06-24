3dc71cd9c65511d5f1d29c153fc3cef75c8491f1
kruskall <99559985+kruskall@users.noreply.github.com>
2025-06-24 02:55:00 +0200

    fix: don't touch go-windows yet

--- metric/system/process/process_windows.go
+++ metric/system/process/process_windows.go
@@ -26,12 +26,12 @@ import (
 	"time"
 	"unsafe"
 
-	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/process"
 	"golang.org/x/sys/windows"
 
 	"github.com/elastic/elastic-agent-libs/opt"
 	"github.com/elastic/elastic-agent-system-metrics/metric/system/resolve"
+	gowindows "github.com/elastic/go-windows"
 )
 
 var (
@@ -391,16 +391,16 @@ func getProcCredName(pid int) (string, error) {
 }
 
 func getIdleProcessTime() (float64, float64, error) {
-	stat, err := cpu.Times(false)
+	idle, kernel, user, err := gowindows.GetSystemTimes()
 	if err != nil {
 		return 0, 0, toNonFatal(err)
 	}
 
 	// Average by cpu because GetSystemTimes returns summation of across all cpus
 	numCpus := float64(runtime.NumCPU())
-	idleTime := stat[0].Idle / numCpus
-	kernelTime := stat[0].System / numCpus
-	userTime := stat[0].User / numCpus
+	idleTime := float64(idle) / numCpus
+	kernelTime := float64(kernel) / numCpus
+	userTime := float64(user) / numCpus
 	// Calculate total CPU time, averaged by cpu
 	totalTime := idleTime + kernelTime + userTime
 	return totalTime, idleTime, nil
